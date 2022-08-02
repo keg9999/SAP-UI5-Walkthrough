5cef5247dc25c4fa3ca14d4b2353e77d7da2b5a2
Dawon Baek <bdw7475@gmail.com>
2022-08-02 13:26:27 +0900

    step3 - Controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,15 @@
 //index.html에서 호출됨
-sap.ui.define([
+//컨트롤: 스크린의 한 부분의 모양과 동작을 정의 하기위해 사용됨. 
 
-], function () {
-    "use strict";
-    alert("UI5 is ready");
+
+sap.ui.define([
+	"sap/m/Text"    //이 컨트롤의 prefix는 네임스페이스 => sap/m
+
+], function (Text) {
+	"use strict";
+
+	new Text({  
+		text: "Hello World"
+	}).placeAt("content");
+
 });
